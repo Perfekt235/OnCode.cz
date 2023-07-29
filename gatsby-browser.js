555f103d6e624d8ae55b299065d1de6658c3488a
OnCode235 <oncode@seznam.cz>
2023-07-29 23:25:10 +0200

    v0.0.1

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,8 +5,8 @@
  */
 
 // You can delete this file if you're not using it
-import "./src/styles/global.css"
+import "./src/styles/global.css";
 import 'normalize.css';
-import './src/styles/scrollbar.css'
+import './src/styles/scrollbar.css';
 
 
