273b2a60535d1ca90ddf4abafe096a4b0b0ca5b1
OnCode235 <oncode@seznam.cz>
2023-08-05 23:50:26 +0200

    Test2

--- src/components/Form/AnalyzeF/AnalyzeForm.js
+++ src/components/Form/AnalyzeF/AnalyzeForm.js
@@ -152,7 +152,7 @@ const AnalyzeForm = () => {
   
     return (
       <>
-        <Form name="Analyze" method='POST' data-netlify="true" >
+        <Form name="Analyze" method='POST' netlify>
           <Input type="hidden" name="form-name" value="contact" />
         
   
